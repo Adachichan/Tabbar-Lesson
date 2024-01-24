ef0de0fba61d4e0311bb64af129253336f5e7e23
Adachichan <129758854+Adachichan@users.noreply.github.com>
2024-01-24 14:39:37 +0000

    [add] #4_Chapter11_script.js jQueryで「クリックイベント」を設定する

---
+++ js/script.js
@@ -0,0 +1,10 @@
+// id = "tab-contents" → class = "tab", id = Not "tab1" → hide
+$('#tab-contents .tab[id != "tab1"]').hide();
+
+$('#tab-menu a').on('click', function(event) {
+  $("#tab-contents .tab").hide();
+  $("#tab-menu .active").removeClass("active");
+  $(this).addClass("active");
+  $($(this).attr("href")).show();
+  event.preventDefault();
+});
